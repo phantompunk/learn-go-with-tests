1ac0523ed757d799a66742627d7340f0a6f3a1d2
Rodrigo Moran <rigo.moran0@gmail.com>
2019-09-12 00:00:27 -0400

    add mocking tests

---
+++ mocking/countdown.go
@@ -0,0 +1,70 @@
+package mocking
+
+import (
+	"fmt"
+	"io"
+	"os"
+	"time"
+)
+
+type Sleeper interface {
+	Sleep()
+}
+
+type SpySleeper struct {
+	Calls int
+}
+
+func (s *SpySleeper) Sleep() {
+	s.Calls++
+}
+
+const write = "write"
+const sleep = "sleep"
+
+type CountdownOperationsSpy struct {
+	Calls []string
+}
+
+func (s *CountdownOperationsSpy) Sleep() {
+	s.Calls = append(s.Calls, sleep)
+}
+
+func (s *CountdownOperationsSpy) Write(p []byte) (n int, err error) {
+	s.Calls = append(s.Calls, write)
+	return
+}
+
+type ConfigurableSleeper struct {
+	duration time.Duration
+	sleep    func(time.Duration)
+}
+
+func (c *ConfigurableSleeper) Sleep() {
+	c.sleep(c.duration)
+}
+
+type SpyTime struct {
+	durationSlept time.Duration
+}
+
+func (s *SpyTime) Sleep(duration time.Duration) {
+	s.durationSlept = duration
+}
+
+const finalWord = "Go!"
+const countDownStart = 3
+
+func Countdown(out io.Writer, sleeper Sleeper) {
+	for index := countDownStart; index > 0; index-- {
+		sleeper.Sleep()
+		fmt.Fprintln(out, index)
+	}
+	sleeper.Sleep()
+	fmt.Fprint(out, finalWord)
+}
+
+func main() {
+	sleeper := &ConfigurableSleeper{1 * time.Second, time.Sleep}
+	Countdown(os.Stdout, sleeper)
+}
